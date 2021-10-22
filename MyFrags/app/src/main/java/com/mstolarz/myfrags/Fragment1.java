4bfc643077175171053976cf62938fe283be41b9
30james00 <48206982+30james00@users.noreply.github.com>
2021-10-22 17:11:43 +0200

    Lab 03

--- MyFrags/app/src/main/java/com/mstolarz/myfrags/Fragment1.java
+++ MyFrags/app/src/main/java/com/mstolarz/myfrags/Fragment1.java
@@ -1,64 +1,66 @@
 package com.mstolarz.myfrags;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+
 public class Fragment1 extends Fragment {
+    //1.
+    public interface OnButtonClickListener {
+        void onButtonClickShuffle();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        void onButtonClickClockwise();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        void onButtonClickHide();
 
-    public Fragment1() {
-        // Required empty public constructor
+        void onButtonClickRestore();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    //2.
+    private OnButtonClickListener callback = null;
+
+    //3.
+    public void setOnButtonClickListener(OnButtonClickListener callback) {
+        this.callback = callback;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        Button buttonShuffle = view.findViewById(R.id.button_shuffle);
+        Button buttonClockwise = view.findViewById(R.id.button_clockwise);
+        Button buttonHide = view.findViewById(R.id.button_hide);
+        Button buttonRestore = view.findViewById(R.id.button_restore);
+
+        buttonShuffle.setOnClickListener(view1 -> {
+            if (callback != null) callback.onButtonClickShuffle();
+        });
+
+        buttonClockwise.setOnClickListener(view12 -> {
+            if (callback != null) callback.onButtonClickClockwise();
+        });
+
+        buttonHide.setOnClickListener(view13 -> {
+            if (callback != null) callback.onButtonClickHide();
+        });
+
+        buttonRestore.setOnClickListener(view14 -> {
+            if (callback != null) callback.onButtonClickRestore();
+        });
+
+        return view;
     }
 }
--- MyFrags/app/src/main/java/com/mstolarz/myfrags/MainActivity.java
+++ MyFrags/app/src/main/java/com/mstolarz/myfrags/MainActivity.java
@@ -1,36 +1,143 @@
 package com.mstolarz.myfrags;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements Fragment1.OnButtonClickListener {
+
+    private int[] frames;
+    private boolean hidden;
+    private int[] sequence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FragmentManager fragmentManager;
-        Fragment fragment1, fragment2, fragment3, fragment4;
-
         setContentView(R.layout.activity_main);
 
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
+        if (savedInstanceState == null) {
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hidden = false;
 
-        fragmentManager = getSupportFragmentManager();
+            sequence = new int[]{0, 1, 2, 3};
 
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            for (int i = 0; i < 4; i++) {
+                transaction.add(frames[i], fragments[i]);
+            }
+            transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hidden = savedInstanceState.getBoolean("HIDDEN");
+            sequence = savedInstanceState.getIntArray("SEQUENCE");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDDEN", hidden);
+        outState.putIntArray("SEQUENCE", sequence);
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+        super.onAttachFragment(fragment);
+        if (fragment instanceof Fragment1) {
+            ((Fragment1) fragment).setOnButtonClickListener(this);
+        }
+    }
+
+    @Override
+    public void onButtonClickShuffle() {
+        List<Integer> s = new ArrayList<>(Arrays.asList(sequence[0], sequence[1], sequence[2], sequence[3]));
+        Collections.shuffle(s);
+        for (int i = 0; i < 4; i++) sequence[i] = s.get(i);
+        newFragments();
+    }
+
+    @Override
+    public void onButtonClickClockwise() {
+        int t = frames[0];
+        frames[0] = frames[1];
+        frames[1] = frames[2];
+        frames[2] = frames[3];
+        frames[3] = t;
+        newFragments();
+    }
+
+    @Override
+    public void onButtonClickHide() {
+        if (hidden) return;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        for (Fragment f : fragmentManager.getFragments()) {
+            if (f instanceof Fragment1) continue;
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.setCustomAnimations(
+                    R.anim.slide_in,  // enter
+                    R.anim.fade_out,  // exit
+                    R.anim.fade_in,   // popEnter
+                    R.anim.slide_out  // popExit
+            );
+            transaction.hide(f);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        hidden = true;
+    }
+
+    @Override
+    public void onButtonClickRestore() {
+        if (!hidden) return;
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frame1, fragment1);
-        transaction.add(R.id.frame2, fragment2);
-        transaction.add(R.id.frame3, fragment3);
-        transaction.add(R.id.frame4, fragment4);
+        transaction.setCustomAnimations(
+                R.anim.slide_in,  // enter
+                R.anim.fade_out,  // exit
+                R.anim.fade_in,   // popEnter
+                R.anim.slide_out  // popExit
+        );
+        for (Fragment f : fragmentManager.getFragments()) {
+            if (f instanceof Fragment1) continue;
+            transaction.show(f);
+        }
         transaction.addToBackStack(null);
         transaction.commit();
+        hidden = false;
+    }
+
+    private void newFragments() {
+        Fragment[] newFragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+        newFragments = new Fragment[]{newFragments[sequence[0]], newFragments[sequence[1]], newFragments[sequence[2]], newFragments[sequence[3]]};
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(
+                R.anim.slide_in,  // enter
+                R.anim.slide_out,  // exit
+                R.anim.slide_in,   // popEnter
+                R.anim.slide_out  // popExit
+        );
+        for (int i = 0; i < 4; i++) {
+            transaction.replace(frames[i], newFragments[i]);
+            if (hidden && !(newFragments[i] instanceof Fragment1))
+                transaction.hide(newFragments[i]);
+        }
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 }
